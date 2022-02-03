11f9c9ec01040bffaf811f388f7c9e3dbcbce58d
rjmandloi-systango <rjmandloi@isystango.com>
2022-02-03 16:42:17 +0530

    some updation

--- index.java
+++ index.java
@@ -9,22 +9,20 @@ import java.io.PrintWriter;
 public class index {
 	public static void main(String[] args) throws IOException {
 		ReadPropertiesFile read = new ReadPropertiesFile();
-		System.out.println(read.checkExistenceOfCsv());
-		boolean checkBlankEntry=false;
+	//	System.out.println(read.checkExistenceOfCsv());
+		boolean checkBlankEntry = false;
 
 		if (read.checkExistenceOfCsv()) {
 			System.out.println("csv file is correct");
 
-			if (CheckForFileFormat.checkColumnsFormatOfInputFile())
-			{
-				
-				BufferedReader reader = new BufferedReader(new FileReader(read.file));
-				String oneEntryFromInput = reader.readLine(); // read line by line data from csv file in string format
-				PrintWriter writer = new PrintWriter(new FileWriter("solutionModify.csv")); // print writer object is used to
-																						// write in destination file
+			if (CheckForFileFormat.checkColumnsFormatOfInputFile()) {
 
+				BufferedReader reader = new BufferedReader(new FileReader(read.file));
+				String oneEntryFromInput = reader.readLine();
+				PrintWriter writer = new PrintWriter(new FileWriter("solutionModify.csv"));
 				int numberOfEntryInInputDataSet = 0;
-				//oneEntryFromInput = reader.readLine();
+				int numberOfColumnInInputCSVFile = 4;
+				int invalidEntry=0;
 				while (oneEntryFromInput != null) {
 
 					if (numberOfEntryInInputDataSet == 0) {
@@ -32,66 +30,58 @@ public class index {
 								+ "Product-SalesTaxAmount" + "," + "Product-FinalPrice" + "," + "Countries");
 						writer.println();
 					} else {
+
 						String productDetails[] = oneEntryFromInput.split(",");
-						//System.out.println(productDetails.length);
-
-						
-						if (productDetails.length <4) {
-							oneEntryFromInput = reader.readLine();
-							continue;
-
-						}
-						
-						for (int count = 0; count < productDetails.length; count++) // checking for null entries in input
-																					// dataset and replace it with 0
-						{
+						for (int count = 0; count < productDetails.length; count++) {
 							if (productDetails[count].isEmpty() || productDetails[count] == null) {
-								productDetails[count] = "0";
-								checkBlankEntry=true;
-                                 																
+
+								checkBlankEntry = true;
+
 							}
 
 						}
-						if(checkBlankEntry)
-						{
-							checkBlankEntry=false;
+
+						if (productDetails.length < numberOfColumnInInputCSVFile || checkBlankEntry) {
+							checkBlankEntry = false;
+							numberOfEntryInInputDataSet++;
+							invalidEntry++;
 							oneEntryFromInput = reader.readLine();
 							continue;
-						}
-						float productCostPrice = Float.parseFloat(productDetails[1].trim());
-						float salesTax = Float.parseFloat(productDetails[2].trim());
-						float productSalesTaxAmount = ((productCostPrice * salesTax) / 100);
-						if (productCostPrice <= 0) // validation for blank entry and negative entries
-						{
-							productCostPrice = 0;
-							productSalesTaxAmount = 0;
-						}
-						
-							
 
-								writer.print(productDetails[0] + "," + productDetails[1] + "," + productDetails[2] + ","
-										+ productSalesTaxAmount + "," + (productCostPrice + productSalesTaxAmount) + ",");
-								writer.print(productDetails[3]);
-							
-								writer.print("undefined");
-						
-							writer.println();
 						}
-						oneEntryFromInput = reader.readLine();
-						numberOfEntryInInputDataSet++;
-					
+						try {
+							
+							float productCostPrice = Float.parseFloat(productDetails[1].trim());
+							float salesTax = Float.parseFloat(productDetails[2].trim());
+							float productSalesTaxAmount = ((productCostPrice * salesTax) / 100);
+							if (productCostPrice <= 0) // validation negative entries
+							{
+								productCostPrice = 0;
+								productSalesTaxAmount = 0;
+							}
+
+							writer.print(productDetails[0] + "," + productDetails[1] + "," + productDetails[2] + ","
+									+ productSalesTaxAmount + "," + (productCostPrice + productSalesTaxAmount) + ",");
+							writer.print(productDetails[3]);
+							writer.println();
+						} catch (NumberFormatException e) {
+							invalidEntry++;
+						}
+					}
+
+					oneEntryFromInput = reader.readLine();
+					numberOfEntryInInputDataSet++;
+
 				}
 				reader.close();
 				writer.flush();
 				writer.close();
-				System.out.println("numberOfEntryInOutputDataSet= "+ (numberOfEntryInInputDataSet - 1));
-			
-			
-				
-				
-				
-			} // column format
-		} // existence
+				System.out.println("total entries in input csv file=" + (numberOfEntryInInputDataSet-1));
+				System.out.println("total invalid entries="+ invalidEntry);
+				System.out.println("total VALID entries="+((numberOfEntryInInputDataSet-1)- invalidEntry));
 
-	} // main method
-} // class
+			}
+		}
+
+	}
+}
