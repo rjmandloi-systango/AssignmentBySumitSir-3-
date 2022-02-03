26cef9a64c7c057d81083a76a4a7423923c1fe8c
rjmandloi-systango <rjmandloi@isystango.com>
2022-02-03 15:05:43 +0530

    README.md file add

---
+++ README.md
@@ -0,0 +1,44 @@
+# Welcome to Tax calculator
+This project will calculate the product price after applying the tax on the actual price of the product and tell you whats the tax over head and the total cost of the product after overhead addition. The user just need to feed one file in CSV formate. The user then in return gets a CSV file which can be opened in the MSExcel or any similar software.  
+
+## Features
+
+- Import the CSV file
+
+## Tech
+- Java
+- IO library
+
+## Installation
+
+Tax calculator requires [Java](https://www.oracle.com/java/technologies/downloads/) and IO library  .
+
+Java I/O (Input and Output) is a library come bundled in jdk.
+
+
+To install java on Linux, just type the following command in the Terminal Window and press Enter.
+
+```sh
+sudo apt install openjdk-8-jdk  
+```
+instead of 8 in the above command you can specify the java version you want.(latest jdk17)
+## Prerequisite:
+SalesTax and ProductCatalog file the file must be in .CSV format,
+SalesTax file must contain column/header name as  "ProductName","ProductCostPrice" ,"ProductSalesTaxPersentage","Country".
+### Note:
+If any cell in the given .csv file contains invalid inputs like character in place of numbers, numbers in place of characters or blank fields then that particular row will not produce any output in the resultant .csv file.
+
+
+## Development
+To contribute on Tax calculator 
+
+```sh
+https://github.com/rjmandloi-systango/AssignmentBySumitSir-3-
+```
+ [Click here](https://github.com/rjmandloi-systango/AssignmentBySumitSir-3-) for contribution
+
+
+
+
+
+enter link description here
--- index.java
+++ index.java
@@ -9,35 +9,87 @@ import java.io.PrintWriter;
 public class index {
 	public static void main(String[] args) throws IOException {
 		ReadPropertiesFile read = new ReadPropertiesFile();
-		ValidationOfData validation = new ValidationOfData();
-//		System.out.println(read.checkExistenceOfCsv());
+		System.out.println(read.checkExistenceOfCsv());
+		boolean checkBlankEntry=false;
 
 		if (read.checkExistenceOfCsv()) {
-//			System.out.println("csv file is correct");
+			System.out.println("csv file is correct");
 
-			if (CheckForFileFormat.checkColumnsFormatOfInputFile()) {
-//				System.out.println("csv format is correct");
+			if (CheckForFileFormat.checkColumnsFormatOfInputFile())
+			{
+				
 				BufferedReader reader = new BufferedReader(new FileReader(read.file));
-				String oneEntryFromInput = reader.readLine();
-				// PrintWriter writer = new PrintWriter(new FileWriter("solutionModify.csv"));
+				String oneEntryFromInput = reader.readLine(); // read line by line data from csv file in string format
+				PrintWriter writer = new PrintWriter(new FileWriter("solutionModify.csv")); // print writer object is used to
+																						// write in destination file
+
 				int numberOfEntryInInputDataSet = 0;
-				oneEntryFromInput = reader.readLine();
-				System.out.println(oneEntryFromInput);
+				//oneEntryFromInput = reader.readLine();
 				while (oneEntryFromInput != null) {
 
-//					if (numberOfEntryInInputDataSet == 0) {
-//						writer.print("Product-Name" + "," + "Product-CostPrice" + "," + "Product-SalesTax" + ","
-//								+ "Product-SalesTaxAmount" + "," + "Product-FinalPrice" + "," + "Countries");
-//						writer.println();
-//
-//					}
-					String productDetails[] = oneEntryFromInput.split(",");
-					validation.validateDataSet(productDetails, numberOfEntryInInputDataSet);
-					// writer.flush();
-					numberOfEntryInInputDataSet++;
-					oneEntryFromInput = reader.readLine();
+					if (numberOfEntryInInputDataSet == 0) {
+						writer.print("Product-Name" + "," + "Product-CostPrice" + "," + "Product-SalesTax" + ","
+								+ "Product-SalesTaxAmount" + "," + "Product-FinalPrice" + "," + "Countries");
+						writer.println();
+					} else {
+						String productDetails[] = oneEntryFromInput.split(",");
+						//System.out.println(productDetails.length);
 
-				} // entry not null
+						
+						if (productDetails.length <4) {
+							oneEntryFromInput = reader.readLine();
+							continue;
+
+						}
+						
+						for (int count = 0; count < productDetails.length; count++) // checking for null entries in input
+																					// dataset and replace it with 0
+						{
+							if (productDetails[count].isEmpty() || productDetails[count] == null) {
+								productDetails[count] = "0";
+								checkBlankEntry=true;
+                                 																
+							}
+
+						}
+						if(checkBlankEntry)
+						{
+							checkBlankEntry=false;
+							oneEntryFromInput = reader.readLine();
+							continue;
+						}
+						float productCostPrice = Float.parseFloat(productDetails[1].trim());
+						float salesTax = Float.parseFloat(productDetails[2].trim());
+						float productSalesTaxAmount = ((productCostPrice * salesTax) / 100);
+						if (productCostPrice <= 0) // validation for blank entry and negative entries
+						{
+							productCostPrice = 0;
+							productSalesTaxAmount = 0;
+						}
+						
+							
+
+								writer.print(productDetails[0] + "," + productDetails[1] + "," + productDetails[2] + ","
+										+ productSalesTaxAmount + "," + (productCostPrice + productSalesTaxAmount) + ",");
+								writer.print(productDetails[3]);
+							
+								writer.print("undefined");
+						
+							writer.println();
+						}
+						oneEntryFromInput = reader.readLine();
+						numberOfEntryInInputDataSet++;
+					
+				}
+				reader.close();
+				writer.flush();
+				writer.close();
+				System.out.println("numberOfEntryInOutputDataSet= "+ (numberOfEntryInInputDataSet - 1));
+			
+			
+				
+				
+				
 			} // column format
 		} // existence
 
